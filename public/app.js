ac2afd194c2878cf3bb5209de103b9b0327bad5f
cymruhangu <pragquix@gmail.com>
2018-09-03 09:31:59 -0400

    initial client side

---
+++ public/app.js
@@ -0,0 +1,166 @@
+//Client javascript
+
+let MOCK_USERS = {
+	"users": [
+        {
+            "id": "1111111",
+            "username": "rnadal",
+            "name": {
+                firstName: 'Rafael',
+                lastName: 'Nadal'
+            },
+            "age": 31,
+            "gender": "male",
+            "ladders": ["singlesOpen"],
+            "dateJoined": 1470016976609,
+            "isActive": true,
+            "matches": []
+        },
+        {
+            "id": "2222222",
+            "username": "rfederer",
+            "name": {
+                firstName: 'Roger',
+                lastName: 'Federer'
+            },
+            "age": 37,
+            "gender": "male",
+            "ladders": ["singlesOpen"],
+            "dateJoined": 1470016976609,
+            "isActive": true,
+            "matches": []
+        },
+        {
+            "id": "333333",
+            "username": "jdelpotro",
+            "name": {
+                firstName: 'Juan Martin',
+                lastName: 'Del Potro'
+            },
+            "age": 29,
+            "gender": "male",
+            "ladders": ["singlesOpen"],
+            "dateJoined": 1470016976609,
+            "isActive": true,
+            "matches": []
+        },
+        {
+            "id": "4444444",
+            "username": "azverev",
+            "name": {
+                firstName: 'Alexander',
+                lastName: 'Zverev'
+            },
+            "age": 20,
+            "gender": "male",
+            "ladders": ["singlesOpen"],
+            "dateJoined": 1470016976609,
+            "isActive": true,
+            "matches": []
+        },
+        {
+            "id": "5555555",
+            "username": "kanderson",
+            "name": {
+                firstName: 'Kevin',
+                lastName: 'Anderson'
+            },
+            "age": 32,
+            "gender": "male",
+            "ladders": ["singlesOpen"],
+            "dateJoined": 1470016976609,
+            "isActive": true,
+            "matches": []
+        },
+        {
+            "id": "6666666",
+            "username": "novak",
+            "name": {
+                firstName: 'Novak',
+                lastName: 'Djokovic'
+            },
+            "age": 31,
+            "gender": "male",
+            "ladders": ["singlesOpen"],
+            "dateJoined": 1470016976609,
+            "isActive": true,
+            "matches": []
+        },
+        {
+            "id": "7777777",
+            "username": "mcilic",
+            "name": {
+                firstName: 'Marin',
+                lastName: 'Cilic'
+            },
+            "age": 28,
+            "gender": "male",
+            "ladders": ["singlesOpen"],
+            "dateJoined": 1470016976609,
+            "isActive": true,
+            "matches": []
+        },
+        {
+            "id": "8888888",
+            "username": "gdimitrov",
+            "name": {
+                firstName: 'Grigor',
+                lastName: 'Dmitrov'
+            },
+            "age": 26,
+            "gender": "male",
+            "ladders": ["singlesOpen"],
+            "dateJoined": 1470016976609,
+            "isActive": true,
+            "matches": []
+        },
+        {
+            "id": "9999999",
+            "username": "dthiem",
+            "name": {
+                firstName: 'Dominic',
+                lastName: 'Thiem'
+            },
+            "age": 22,
+            "gender": "male",
+            "ladders": ["singlesOpen"],
+            "dateJoined": 1470016976609,
+            "isActive": true,
+            "matches": []
+        },
+        {
+            "id": "10000000",
+            "username": "dgoffin",
+            "name": {
+                firstName: 'David',
+                lastName: 'Goffin'
+            },
+            "age": 27,
+            "gender": "male",
+            "ladders": ["singlesOpen"],
+            "dateJoined": 1470016976609,
+            "isActive": true,
+            "matches": []
+        },
+        {
+            "id": "110000000",
+            "username": "jisner",
+            "name": {
+                firstName: 'John',
+                lastName: 'Isner'
+            },
+            "age": 33,
+            "gender": "male",
+            "ladders": ["singlesOpen"],
+            "dateJoined": 1470016976609,
+            "isActive": true,
+            "matches": []
+        }
+    ]
+};
+
+let MOCK_MATCHES = {
+    "matches":[
+        
+    ]
+};
