91ed333a648bc4300975ce08bec1cfd977866fd3
cymruhangu <pragquix@gmail.com>
2018-10-23 16:10:07 -0400

    removed local db vars from config.js

--- config.js
+++ config.js
@@ -1,8 +1,5 @@
 'use strict';
 
-// exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost:27017/tennis-ladder-app";
-// // exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || "mongodb://localhost:27017/test-tennis-ladder-app";
-
 exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://dbadmin:cde32wsxzaq1@ds147592.mlab.com:47592/tennis-ladder-app";
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || "mongodb://dbadmin:cde32wsxzaq1@ds245762.mlab.com:45762/test-tennis-ladder-app";
 exports.PORT = process.env.PORT || 8080;
